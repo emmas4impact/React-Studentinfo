b9fd776f923e300aae9661e45051b805ff972a8c
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-07-28 17:51:33 +0300

    Create Student.jsx

---
+++ studentinfo/src/Component/Student.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react' 
+import {Button, Row, Col} from 'react-bootstrap';
+
+class Student extends Component{
+    state={
+        
+        
+        
+    }
+    
+    render(){
+        return(
+            
+            
+        )
+    }
+    
+}
+
+export default Student
