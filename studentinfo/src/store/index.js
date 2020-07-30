6dd879ee3901e71e3420ef4db9850f72c30d44d5
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-07-30 16:57:35 +0300

    redux store

---
+++ studentinfo/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from "redux";
+import rootReducer from "../reducer";
+
+const initialState = {
+    isLoading: true,
+  
+};
+
+export default function configureStore() {
+  return createStore(
+    rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+}
