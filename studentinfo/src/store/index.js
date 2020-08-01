844a658f395c27a10ac6966eb8e5a0bc6a6429ea
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-08-01 21:15:56 +0300

    Update index.js

--- studentinfo/src/store/index.js
+++ studentinfo/src/store/index.js
@@ -1,15 +1,49 @@
-import { createStore } from "redux";
-import rootReducer from "../reducer";
+import { createStore, combineReducers, compose, applyMiddleware  } from "redux";
+//import rootReducer from "../reducer";
+import dataReducer from '../reducer/data'
+import statusReducer from '../reducer/status'
+import thunk from "redux-thunk";
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const initialState = {
-    isLoading: true,
+    status:{
+        isLoading: true,
+        errMess: ""
+    },
+    data:{
+        students: [],
+        projects: [],
+        Total: 0,
+        totalProject: 0,
+        limit: 5,
+        show: false,
+        student:{
+            firstname: "",
+            surname: "",
+            email: "",
+            dateofbirth: ""
+        },
+        project:{
+            projectname: "",
+            description: "",
+            repourl: "",
+            liveurl: "",
+            studentid: 0
+        },
+        
+        
+    }
+  
+   
   
 };
+const bigReducer = combineReducers({ status: statusReducer, data: dataReducer });
 
 export default function configureStore() {
   return createStore(
-    rootReducer,
+    bigReducer,
     initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(thunk))
   );
 }
